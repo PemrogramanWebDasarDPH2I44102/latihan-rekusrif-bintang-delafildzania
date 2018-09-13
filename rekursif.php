dc1a702d98943679098ab0b5465e3982996add60
delafildzania <42663150+delafildzania@users.noreply.github.com>
2018-09-13 09:00:19 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,5 +1,5 @@
 <?php
-
+echo"<b>Bintang Piramida dengan menggunakan fungsi Rekursif</b><br><br>";
 function rekursif ($batas){
 	echo "<pre>";
 	for ($i=1;$i<=$batas;$i++){
@@ -11,4 +11,27 @@ function rekursif ($batas){
 	echo "</pre>";
 }
 rekursif(5);
+echo"<br>=================================";
+
+echo"<b>Piramida Angka dengan menggunakan fungsi Rekursif</b><br><br>";
+
+function rekursif2 ($batas){
+	echo "<pre>";
+	for ($i=1; $i <=5 ; $i++) {
+ for ($j=4; $j>=$i ; $j--) {
+ echo "&nbsp";
+ }
+for ($k=$i; $k >=1 ; $k--) {
+ echo "$k";
+ }
+ echo "<br>";
+}
+echo"<pre>";
+ 
+}
+
+rekursif2(5);
+
 ?>
+
+
